02f40eaceb5bf0b5bc06f2029c3c415e2e88b9ac
jorgeMunozMartinez <32263233+jorgeMunozMartinez@users.noreply.github.com>
2021-05-01 20:05:44 +0200

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python3
 
+import requests
+
+def check(url):
+   x = requests.get('https://www.uclm.com/%27)
+
+   if x.status_code == 200:
+     print('yay!')
+     return 'yay!'
+   else:
+     print('uh-oh!')
+     return 'error'
